0de9d1d293527776374141dc288fabcd64799f78
Seokyong Shin <kistssy@gmail.com>
2022-04-06 15:32:25 +0900

    main: Change bach size

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ transform = torchvision.transforms.Compose([
     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 trainset = torchvision.datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = torchvision.models.resnet18(pretrained=True).to(device)
